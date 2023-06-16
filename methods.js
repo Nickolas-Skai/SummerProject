aa9ec3718a94e1990c384f9a4503b9adb66d3bc0
Nickolas-Skai <89269942+Nickolas-Skai@users.noreply.github.com>
2023-06-15 18:16:08 -0600

    Add files via upload

--- methods.js
+++ methods.js
@@ -1,1 +1,17 @@
-// java is linked to the  html file 
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://forecast9.p.rapidapi.com/status/',
+  headers: {
+    'X-RapidAPI-Key': 'cd636672demsh288f2093d232388p137931jsn21216d2ad9f4',
+    'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}
