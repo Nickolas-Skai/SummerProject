6eaebe8768cc2c7b52cc23674f2a84ace8e64c02
Nickolas-Skai <89269942+Nickolas-Skai@users.noreply.github.com>
2023-06-16 23:25:36 -0600

    Add files via upload

--- methods.js
+++ methods.js
@@ -1,17 +1,24 @@
-const axios = require('axios');
 
-const options = {
-  method: 'GET',
-  url: 'https://forecast9.p.rapidapi.com/status/',
-  headers: {
-    'X-RapidAPI-Key': 'cd636672demsh288f2093d232388p137931jsn21216d2ad9f4',
-    'X-RapidAPI-Host': 'forecast9.p.rapidapi.com'
-  }
-};
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-try {
-	const response = await axios.request(options);
-	console.log(response.data);
-} catch (error) {
-	console.error(error);
-}
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAJoRtTkTVbcnaFeR9Yhqqz3We6pk8udp8",
+    authDomain: "summerproject-5ad92.firebaseapp.com",
+    projectId: "summerproject-5ad92",
+    storageBucket: "summerproject-5ad92.appspot.com",
+    messagingSenderId: "474443637768",
+    appId: "1:474443637768:web:34b2c432652e3f4ff81dbd",
+    measurementId: "G-TZ58VRF047"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  
